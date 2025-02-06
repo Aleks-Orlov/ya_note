df08bdf5675fcc3c3fda8e75514b73017e83a2db
Aleks-Orlov <lexx4432@ya.ru>
2025-02-07 00:48:31 +0300

    Add test_logic

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -13,6 +13,7 @@ User = get_user_model()
 
 
 class TestCommentCreation(TestCase):
+    NOTE_TITLE = 'Заголовок заметки'
     NOTE_TEXT = 'Текст заметки'
 
     @classmethod
@@ -28,11 +29,11 @@ class TestCommentCreation(TestCase):
         cls.url = reverse('notes:add', args=None)
         # логинимся в клиенте.
         cls.auth_client = Client()
-        cls.auth_client.force_login(cls.user)
+        cls.auth_client.force_login(cls.author)
         # Данные для POST-запроса при создании комментария.
         cls.form_data = {
-            'title': 'Заголовок',
-            'text': 'Текст',
+            'title': cls.NOTE_TITLE,
+            'text': cls.NOTE_TEXT,
             'slug': 'sample',
         }
         # 'text': cls.COMMENT_TEXT}
@@ -45,3 +46,19 @@ class TestCommentCreation(TestCase):
         notes_count = Note.objects.count()
         # Ожидаем, что комментариев в базе нет - сравниваем с нулём.
         self.assertEqual(notes_count, 0)
+
+    def test_user_can_create_comment(self):
+        # Совершаем запрос через авторизованный клиент.
+        response = self.auth_client.post(self.url, data=self.form_data)
+        # Проверяем, что редирект привёл к разделу с комментами.
+        # self.assertRedirects(response, self.url)
+        # Считаем количество комментариев.
+        notes_count = Note.objects.count()
+        # Убеждаемся, что есть один комментарий.
+        self.assertEqual(notes_count, 1)
+        # Получаем объект комментария из базы.
+        note = Note.objects.get()
+        # Проверяем, что все атрибуты комментария совпадают с ожидаемыми.
+        self.assertEqual(note.title, self.NOTE_TITLE)
+        self.assertEqual(note.text, self.NOTE_TEXT)
+        self.assertEqual(note.author, self.author)
