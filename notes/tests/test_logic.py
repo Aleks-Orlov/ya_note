60be2bff4eb2eb5608330e3c969195739e1bdcc0
Aleks-Orlov <lexx4432@ya.ru>
2025-02-13 22:32:04 +0300

    Add tests delete notes

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -12,22 +12,15 @@ from notes.models import Note
 User = get_user_model()
 
 
-class TestCommentCreation(TestCase):
+class TestNoteCreation(TestCase):
     NOTE_TITLE = 'Заголовок заметки'
     NOTE_TEXT = 'Текст заметки'
     WARNING = ' - такой slug уже существует, придумайте уникальное значение!'
-    SLUG_FOR_SLUGIFY = "СЛАГ"
 
     @classmethod
     def setUpTestData(cls):
         cls.user = User.objects.create(username='testUser')
         cls.author = User.objects.create(username='Автор заметки')
-        # cls.notes = Note.objects.create(
-        #     title='Заголовок',
-        #     text='Текст',
-        #     slug='sample',
-        #     author=cls.author
-        # )
         cls.url = reverse('notes:add', args=None)
         # логинимся в клиенте.
         cls.auth_client = Client()
@@ -38,7 +31,6 @@ class TestCommentCreation(TestCase):
             'text': cls.NOTE_TEXT,
             'slug': 'sample',
         }
-        # 'text': cls.COMMENT_TEXT}
 
     def test_anonymous_user_cant_create_note(self):
         # Совершаем запрос от анонимного клиента, в POST-запросе отправляем
@@ -49,11 +41,9 @@ class TestCommentCreation(TestCase):
         # Ожидаем, что комментариев в базе нет - сравниваем с нулём.
         self.assertEqual(notes_count, 0)
 
-    def test_user_can_create_comment(self):
+    def test_user_can_create_note(self):
         # Совершаем запрос через авторизованный клиент.
         self.auth_client.post(self.url, data=self.form_data)
-        # Проверяем, что редирект привёл к разделу с комментами.
-        # self.assertRedirects(response, self.url)
         # Считаем количество записей.
         notes_count = Note.objects.count()
         # Убеждаемся, что есть один комментарий.
@@ -77,8 +67,8 @@ class TestCommentCreation(TestCase):
         self.assertFormError(
             response,
             form='form',
-            field='text',
-            errors=f"{self.form_data['slug']}WARNING"
+            field='slug',
+            errors=f"{self.form_data['slug']}{self.WARNING}"
         )
         # Дополнительно убедимся, что комментарий не был создан.
         comments_count = Note.objects.count()
@@ -92,3 +82,40 @@ class TestCommentCreation(TestCase):
         )
         note = Note.objects.get()
         self.assertEqual(note.slug, slugify(self.NOTE_TITLE))
+
+
+class TestNoteDelete(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(username='testUser')
+        cls.user_client = Client()
+        cls.user_client.force_login(cls.user)
+        cls.author = User.objects.create(username='Автор заметки')
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+        cls.notes = Note.objects.create(
+            title='Заголовок',
+            text='Текст',
+            slug='sample',
+            author=cls.author
+        )
+        # URL для удаления комментария.
+        cls.delete_url = reverse('notes:delete', args=(cls.notes.slug,))
+
+    def test_author_can_delete_note(self):
+        # От имени автора комментария отправляем DELETE-запрос на удаление.
+        self.author_client.delete(self.delete_url)
+        # Считаем количество комментариев в системе.
+        notes_count = Note.objects.count()
+        # Ожидаем ноль комментариев в системе.
+        self.assertEqual(notes_count, 0)
+
+    def test_user_cant_delete_comment_of_another_user(self):
+        # Выполняем запрос на удаление от пользователя-читателя.
+        response = self.user_client.delete(self.delete_url)
+        # Проверяем, что вернулась 404 ошибка.
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        # Убедимся, что комментарий по-прежнему на месте.
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 1)
